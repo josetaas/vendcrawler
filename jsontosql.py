678ba5e332b7c54cdb39d117dc443bf88da7d111
Jose Francisco Taas <josetaas@gmail.com>
2016-04-21 16:00:07 +0800

    Add json to sql conversion script.

---
+++ jsontosql.py
@@ -0,0 +1,45 @@
+import os
+import os.path
+from json import loads
+
+import click
+
+from vendcrawler.scripts.vendcrawlerdb import VendCrawlerDB
+
+class JSONToSQL(object):
+
+    def __init__(self, json, user, password, database):
+        self.data = loads(json.read())
+        self.db = VendCrawlerDB(user, password, database)
+
+        
+        table = 'items'
+        columns = ['item_id', 'item_name', 'vendor_id', 'shop_name',
+                   'amount', 'price', 'map', 'datetime']
+        values = []
+        for items in data:
+            for item in items:
+                value = [int(item['id']),
+                        item['name'],
+                        int(item['vendor_id']),
+                        item['shop'], 
+                        int(item['amount'].replace(',', '')),
+                        int(item['price'].replace(',', '')),
+                        item['map'],
+                        item['datetime']]
+
+                values.append(value)
+
+        self.vcdb.insert(table, columns, values)
+
+
+@click.command()
+@click.argument('json', type=click.File('r'))
+@click.argument('user')
+@click.argument('password')
+@click.argument('database')
+def cli(json, user, password, database):
+    JSONToSQL(json, user, password, database)
+
+if __name__ == '__main__':
+    cli()
