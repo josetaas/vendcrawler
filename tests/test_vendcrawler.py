82b7e46ebdeb154963520fec1d41cc624ceb806d
Jose Francisco Taas <josetaas@gmail.com>
2016-04-21 15:03:03 +0800

    Fix test by passing placeholder variables.

--- tests/test_vendcrawler.py
+++ tests/test_vendcrawler.py
@@ -5,7 +5,7 @@ from vendcrawler.scripts.vendcrawler import VendCrawler
 class TestVendCrawlerMethods(unittest.TestCase):
 
     def test_get_links(self):
-        links = VendCrawler().get_links(2)
+        links = VendCrawler('a', 'b', 'c').get_links(2)
         self.assertEqual(links, 
                          ['https://sarahserver.net/?module=vendor&p=1',
                           'https://sarahserver.net/?module=vendor&p=2'])
@@ -13,7 +13,7 @@ class TestVendCrawlerMethods(unittest.TestCase):
     def test_get_page_count(self):
         with open('test_vendcrawler.html', 'r') as f:
             data = f.read()
-        page_count = VendCrawler().get_page_count(str(data))
+        page_count = VendCrawler('a', 'b', 'c').get_page_count(str(data))
         self.assertEqual(int(page_count), 84)
 
 if __name__ == '__main__':
