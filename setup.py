13bf1f177c5dd6435eef957a5be2b3f886ed3c5e
Jose Francisco Taas <josetaas@gmail.com>
2016-04-21 12:32:57 +0800

    Remove readme.

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from codecs import open as codecs_open
 from setuptools import setup, find_packages
 
 
-# Get the long description from the relevant file
-with codecs_open('README.rst', encoding='utf-8') as f:
-    long_description = f.read()
-
-
 setup(name='vendcrawler',
       version='0.0.1',
       classifiers=[],
